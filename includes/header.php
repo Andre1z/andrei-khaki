7f9d71659bf21be4653d69795e0cd5e1817a2cc7
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-04-30 19:18:35 +0200

    añadimos dentro de header.php el contenido

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario - Andrei Khaki</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<header>
+    <h1>Calendario Personalizado</h1>
+</header>

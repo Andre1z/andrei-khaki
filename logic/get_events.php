c4b701c84b3cc6711ef27c41101dba187b687627
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-15 20:17:38 +0200

    Update get_events.php

--- logic/get_events.php
+++ logic/get_events.php
@@ -4,10 +4,25 @@ include '../includes/db_config.php';
 $query = "SELECT identificador, titulo AS title, descripcion AS description, fecha_inicio AS start, fecha_fin AS end, categoria FROM eventos";
 $resultado = $conexion->query($query);
 
-$events = [];
-while ($evento = $resultado->fetch_assoc()) {
-    $events[] = $evento;
-}
+echo "<table border='1'>";
+echo "<tr>
+    <th>Identificador</th>
+    <th>Título</th>
+    <th>Descripción</th>
+    <th>Fecha Inicio</th>
+    <th>Fecha Fin</th>
+    <th>Categoría</th>
+      </tr>";
 
-echo json_encode($events);
+while ($evento = $resultado->fetch_assoc()) {
+    echo "<tr>
+        <td>{$evento['identificador']}</td>
+        <td>{$evento['title']}</td>
+        <td>{$evento['description']}</td>
+        <td>{$evento['start']}</td>
+        <td>{$evento['end']}</td>
+        <td>{$evento['categoria']}</td>
+      </tr>";
+}
+echo "</table>";
 ?>
